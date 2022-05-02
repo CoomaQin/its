33fceaffce5421936644e01b20e38e55d1bf2972
bustercaj <49170225+bustercaj@users.noreply.github.com>
2022-05-02 12:17:15 -0400

    Modified save format of key point
    Separate the key point into different columns for easy to import into oracle

--- ITSproject.py
+++ ITSproject.py
@@ -13,25 +13,29 @@ with open('kp.npy', 'rb') as f:
     kp = np.load(f, allow_pickle = True)
 with open('od_info.npy', 'rb') as f:
     od = np.load(f, allow_pickle = True)
-
+number_of_lane = len(max(kp, key = len))
 timestamp = pd.read_csv('Timestamp.csv')
-
-
 a = np.arange(1630098945367,1630099005334,1/3*100)
+#df = pd.DataFrame({'frame':a, 'key point':kp, 'bounding box': od})
 
-df = pd.DataFrame({'frame':a, 'key point':kp, 'bounding box': od})
+#print(kp[0][0])
 
 timestamp['frame'] = 0
-timestamp['key point'] = None
 timestamp['bounding box'] = None
-
-
+for j in range(number_of_lane):
+    name = 'key point ' + str(j)
+    timestamp[name] = None
 
 for i in range(len(timestamp)):
     index = np.argmin(abs(timestamp['timestamp'][i] - a))
     timestamp['frame'][i] = index
-    timestamp['key point'][i] = df['key point'][index]
-    timestamp['bounding box'][i] = df['bounding box'][index]
-
+#    timestamp['key point'][i] = df['key point'][index]
+    timestamp['bounding box'][i] = od[index]
+    for j in range(number_of_lane):
+        name = 'key point ' + str(j)
+        try:
+            timestamp[name][i] = kp[index][j]
+        except Exception:
+            timestamp[name][i] = np.NaN
 
 timestamp.to_csv('Timestamp_Frame.csv')

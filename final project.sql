4c39eeb16c0bf108477f16edc34252cc70330e69
bustercaj <49170225+bustercaj@users.noreply.github.com>
2022-05-04 16:10:23 -0400

    Update database query

--- final project.sql
+++ final project.sql
@@ -62,3 +62,45 @@ right_bottom_y number,
 veh_loaction  varchar2(4000)
 );
 select * from smartmobi_log_bounding_box;
+
+
+--case 1
+Select unix_ts, frame, vehicle_index, veh_loaction
+from smartmobi_log_bounding_box
+where veh_loaction = 'current' and frame <= 450
+order by frame asc;
+
+Select vehicle_index
+from smartmobi_log_bounding_box
+where veh_loaction = 'current' and frame <= 450
+Group by vehicle_index;
+
+
+
+--case 2
+Select count(smartmobi_log.unix_ts) as total_row,  count(smartmobi_log_bounding_box.unix_ts) as veh_2_occur_row
+from smartmobi_log
+left join smartmobi_log_bounding_box
+on smartmobi_log.unix_ts = smartmobi_log_bounding_box.unix_ts
+where smartmobi_log_bounding_box.vehicle_index = 2 and smartmobi_log.unix_ts <= 1630098960367;
+
+Select distinct(veh_loaction), vehicle_index
+from smartmobi_log_bounding_box
+where vehicle_index = 2 and unix_ts <= 1630098960367;
+
+
+
+
+--case 3
+create or replace view speed_table as
+SELECT smartmobi_log.unix_ts, smartmobi_log.frame, smartmobi_log.speed, 
+smartmobi_log_bounding_box.unix_ts AS BD_UNIX_TS, smartmobi_log_bounding_box.frame BD_Frame, smartmobi_log_bounding_box.vehicle_index, smartmobi_log_bounding_box.veh_loaction,
+smartmobi_log.speed - (LAG(smartmobi_log.speed) OVER (ORDER BY smartmobi_log_bounding_box.frame)) as delta_speed
+FROM smartmobi_log
+LEFT JOIN smartmobi_log_bounding_box
+ON smartmobi_log.unix_ts = smartmobi_log_bounding_box.unix_ts
+where vehicle_index = 2 and smartmobi_log.frame <= 450
+ORDER BY smartmobi_log_bounding_box.frame ASC;
+
+select * from speed_table
+where delta_speed <>0 or delta_speed is null;
